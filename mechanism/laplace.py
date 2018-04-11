df0ce52cfcf299238e8b0ad9e3a585bcbbe619b6
mibarg <michaelbargury@gmail.com>
2018-04-12 00:00:35 +0300

    laplace docs

--- mechanism/laplace.py
+++ mechanism/laplace.py
@@ -5,6 +5,19 @@ from data.query import Query
 
 
 def laplace(db, query, eps=1e-5):
+    """
+    Goal:
+       The Laplace mechanism is a DP method to answer numeric queries,
+       by adding Laplace[s(func)/eps] distributed noise to the true answer
+    Guaranties:
+    1. (eps,0) Differential Privacy
+    2. Accuracy:
+       Let func: N ^ |Universe| -> R^k
+       Denote y = laplace(db, func, eps)
+              s = func.sensitivity
+       Then forall d in (0,1]
+              P[max_i |f(x)_i - y_i| >= log(k/d)(s/eps)] <= d
+    """
 
     assert (db.rep == 'histogram')
     assert (isinstance(query, Query))
